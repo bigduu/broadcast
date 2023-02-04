7a98d13d8ec7100ca0155ae030d799407e71792c
bigduu <mugeng.du@qq.com>
2023-02-04 22:17:14 +0800

    network format

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -3,6 +3,7 @@ use std::net::IpAddr;
 
 use local_ip_address::list_afinet_netifas;
 use serde::{Deserialize, Serialize};
+use tracing::error;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct NetworkInterface {
@@ -18,10 +19,13 @@ impl NetworkInterface {
 
 // List all local ipv4 ip addresses.
 pub fn list_ipv4_addresses() -> Vec<NetworkInterface> {
-    let network_interfaces = list_afinet_netifas().expect("Failed to list network interfaces");
-    network_interfaces
+    list_afinet_netifas()
+        .unwrap_or_else(|e| {
+            error!("list_afinet_netifas error: {}", e);
+            Vec::new()
+        })
         .iter()
         .filter(|(_name, ip)| ip.is_ipv4())
-        .map(|(name, ip)| NetworkInterface::new(name.to_string(), *ip))
+        .map(|(name, ip)| NetworkInterface::new(name.clone(), *ip))
         .collect()
 }
